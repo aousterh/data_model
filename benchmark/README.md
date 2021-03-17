82b9eda5742a0c3540ef7563a83aa37eab108133
aousterh <amy.ousterhout@berkeley.edu>
2021-03-17 01:40:16 +0000

    benchmark: add jq and zq queries to benchmarks

--- benchmark/README.md
+++ benchmark/README.md
@@ -1,10 +1,11 @@
 Benchmark
 ==
 
-The values are average across 10 runs and in second.
+The values are averages across 10 runs and in seconds.
 
 Notes:
 * For pandas, we do not acount for the time to load/dump data from/to disk. So the input format and output format represent the "in-memory format" only.
+* zq and jq results are for a single run.
 
 ### zq-sample-data
 |**<br>Tool**|**<br>Queries**|**Input<br>Format**|**Output<br>Format**|**<br>Real**|**<br>User**|**<br>Sys**|
@@ -16,3 +17,9 @@ Notes:
 | `spark` | `analytics` | dataframe | dataframe | 6.24149 | 0.00267| 0.00133 |
 | `spark` | `search` | dataframe | dataframe | 34.93111 | 0.004 | 0.00133 |
 | `spark` | `discovery` | dataframe | dataframe | 6.45012 | 0.052 | 0.004 |
+| `zq` | `analytics` | zng | zng | 2.27 | 3.74 | 0.09 |
+| `zq` | `search` | zng | zng | 2.04 | 3.37 | 0.13 |
+| `zq` | `discovery` | zng | zng | 2.23 | 3.67 | 0.10 |
+| `jq` | `analytics` | ndjson | ndjson | 35.28 | 36.50 | 2.72 |
+| `jq` | `search` | ndjson | ndjson | 26.09 | 27.48 | 2.61 |
+| `jq` | `discovery` | ndjson | ndjson | 32.59 | 33.76 | 2.80 |
---
+++ benchmark/z_benchmark.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# shellcheck disable=SC2016    # The backticks in quotes are for markdown, not expansion
+# based off of https://github.com/brimsec/zq/blob/master/scripts/comparison-test
+
+
+set -eo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
+# assume volume is mounted at /zq-sample-data
+BASE_DIR="/zq-sample-data"
+DATA=$BASE_DIR"/z"
+DATA_NDJSON=$BASE_DIR"/ndjson/zeek-ndjson"
+DATA_NDJSON_NESTED=$BASE_DIR"/ndjson-nested"
+
+if [[ $(type -P "gzcat") ]]; then
+  ZCAT="gzcat"
+elif [[ $(type -P "zcat") ]]; then
+  ZCAT="zcat"
+else
+  echo "gzcat/zcat not found in PATH"
+  exit 1
+fi
+
+for CMD in zq jq; do
+  if ! [[ $(type -P "$CMD") ]]; then
+    echo "$CMD not found in PATH"
+    exit 1
+  fi
+done
+
+declare -a MARKDOWNS=(
+#    '01_all_unmodified.md'
+#    '02_cut_ts.md'
+#    '03_count_all.md'
+    '04_count_by_id_orig_h.md'
+#    '05_only_id_resp_h.md'
+    '06_search_top_5.md'
+    '07_count_by_type.md'
+)
+
+declare -a DESCRIPTIONS=(
+#    'Output all events unmodified'
+#    'Extract the field `ts`'
+#    'Count all events'
+    'Count all events, grouped by the field `id.orig_h`'
+#    'Output all events with the field `id.resp_h` set to `52.85.83.116`'
+    'Top 5 events with `id.orig_h` set to `10.128.0.19`'
+    'Count all events, grouped by type'
+)
+
+declare -a ZQL_QUERIES=(
+#    '*'
+#    'cut ts'
+#    'count()'
+    'count() by id.orig_h'
+#    'id.resp_h=52.85.83.116'
+    'id.orig_h=10.128.0.19 | sort ts | head 5'
+    'count() by typeof(.)'
+)
+
+declare -a JQ_FILTERS=(
+#    '.'
+#    '. | { ts }'
+#    '. | length'
+    'group_by(."id.orig_h")[] | {"count": length, "id.orig_h": .[0]."id.orig_h"}'
+#    '. | select(.["id.resp_h"]=="52.85.83.116")'
+    '[ .[] | select(.["id.orig_h"]=="10.128.0.19") ] | sort_by(.ts) | .[:5] | .[]'
+    'group_by(."_path")[] | {"count": length, "_path": .[0]."_path"}'
+)
+
+declare -a JQFLAGS=(
+#    '-c'
+#    '-c'
+#    '-c -s'
+    '-c -s'
+#    '-c'
+    '-c -s'
+    '-c -s'
+)
+
+#INPUT_FORMATS=(zng zng-uncompressed zson tzng ndjson)
+#OUTPUT_FORMATS=(zng zng-uncompressed zson tzng ndjson)
+INPUT_FORMATS=(zng zng-uncompressed zst)
+OUTPUT_FORMATS=(zng zng-uncompressed zst)
+for (( n=0; n<"${#ZQL_QUERIES[@]}"; n++ ))
+do
+    DESC=${DESCRIPTIONS[$n]}
+    MD=${MARKDOWNS[$n]}
+    zql=${ZQL_QUERIES[$n]}
+    echo -e "### $DESC\n" | tee "$MD"
+    echo "|**<br>Tool**|**<br>Arguments**|**Input<br>Format**|**Output<br>Format**|**<br>Real**|**<br>User**|**<br>Sys**|" | tee -a "$MD"
+    echo "|:----------:|:---------------:|:-----------------:|:------------------:|-----------:|-----------:|----------:|" | tee -a "$MD"
+    for INPUT in ${INPUT_FORMATS[@]} ; do
+      for OUTPUT in ${OUTPUT_FORMATS[@]} ; do
+        echo -n "|\`zq\`|\`$zql\`|$INPUT|$OUTPUT|" | tee -a "$MD"
+        if [[ $INPUT == "zng-uncompressed" ]]; then
+          INPUT_FMT="zng"
+        else
+          INPUT_FMT="$INPUT"
+        fi
+        if [[ $OUTPUT == "zng-uncompressed" ]]; then
+          ALL_TIMES=$(time -p (zq -i "$INPUT_FMT" -f zng -znglz4blocksize 0 "$zql" $DATA/$INPUT/* > /dev/null) 2>&1)
+        else
+          ALL_TIMES=$(time -p (zq -i "$INPUT_FMT" -f "$OUTPUT" "$zql" $DATA/$INPUT/* > /dev/null) 2>&1)
+        fi
+        echo "$ALL_TIMES" | tr '\n' ' ' | awk '{ print $2 "|" $4 "|" $6 "|" }' | tee -a "$MD"
+      done
+    done
+
+    for NDJSON_DATA in $DATA_NDJSON $DATA_NDJSON_NESTED; do
+	if [[ $NDJSON_DATA == $DATA_NDJSON ]]; then
+	    FMT="ndjson flat"
+	else
+	    FMT="ndjson nested"
+	fi
+	
+        JQ=${JQ_FILTERS[$n]}
+        JQFLAG=${JQFLAGS[$n]}
+        echo -n "|\`jq\`|\`$JQFLAG ""'""${JQ//|/\\|}""'""\`|$FMT|$FMT|" | tee -a "$MD"
+        # shellcheck disable=SC2086      # For expanding JQFLAG
+        ALL_TIMES=$(time -p ($ZCAT "$NDJSON_DATA"/*.gz | jq $JQFLAG "$JQ" > /dev/null) 2>&1)
+        echo "$ALL_TIMES" | tr '\n' ' ' | awk '{ print $2 "|" $4 "|" $6 "|" }' | tee -a "$MD"
+    done
+
+    echo | tee -a "$MD"
+done
