be58f6f420c7799fa7263e895cbc50184fb79406
autumnli11 <qiushiautumnli@gmail.com>
2021-05-19 15:32:11 +0000

    benchmark

--- queries/spark_benchmark.py
+++ queries/spark_benchmark.py
@@ -10,16 +10,20 @@ import os
 from collections import defaultdict
 import statistics
 from pyspark import SparkConf
+import sys
+import time
+from statistics import mean
 
 merged_df_path = '/home/admin/zq-sample-data/outputs/merged'
 parquet_path = '/home/admin/zq-sample-data/parquet'
 
 #conf = SparkConf().set("spark.executor.memory","2g").set("spark.driver.memory","2g").setMaster("local")
-sc = SparkContext("local")
+#sc = SparkContext("local")
 #sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
+#sqlContext = SQLContext(sc)
 #spark = SparkSession.builder.master("local[1]").config("spark.driver.memory","2g").config("spark.executor.memory","2g").getOrCreate()
-spark = SparkSession.builder.master("local[1]").getOrCreate()
+spark = SparkSession.builder.master("local[1]").config("spark.ui.port","4050").getOrCreate()
+
 
 def unix_time(function, *args, **kwargs):
     '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
@@ -47,12 +51,12 @@ def has_column(df, col):
 
 #load the merged parquet into a df
 def load():
-    df = sqlContext.read.parquet(merged_df_path)
+    df = spark.read.parquet(merged_df_path)
     return df
 
-def analytics(df):   
-  #df.groupBy("`id.orig_h`").count().show(df.count(), False)
-  df.select("`id.orig_h`").count()
+def analytics(df, iter_num):   
+   # df.groupBy("`id.orig_h`").count().show(df.count(), False
+   df.select("`id.orig_h`").count()
   
   #over each individual parquet instead of the merged file
   #for root, dirs, files in os.walk(parquet_path, topdown = False):
@@ -60,22 +64,20 @@ def analytics(df):
     #      df = spark.read.parquet(os.path.join(root, name))
      #     if has_column(df, "`id.orig_h`"):
       #        df.select("`id.orig_h`").count()
-          
 
-
-
-
-def search(df):
-
-    df2 = spark.sql("""SELECT * 
+def search(df, iter_num):
+   origin = sys.stdout
+   df2 = spark.sql("""SELECT *
     FROM MERGED
     WHERE `id.orig_h` = '10.128.0.19'
     ORDER BY ts
     LIMIT 5
     """)
-    df2.count()
+   sys.stdout = open('/dev/null', 'w')
+   df2.show()
+   sys.stdout = origin
 
-def discovery(df):
+def discovery(df, iter_num):
     for root, dirs, files in os.walk(parquet_path, topdown = False):
         for name in files:
            # print(name)
@@ -83,44 +85,154 @@ def discovery(df):
             df.createOrReplaceTempView("Schema")
             df2 = spark.sql("""SELECT COUNT(*) FROM Schema""")
             df2.count()
+    
+def path(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT * FROM MERGED
+     WHERE _path = 'smb*' or _path = 'dce_rpc' """)
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
 
-def benchmark1(fn, df, num_iter=10):
-    #warm up run
-    #fn(df)
+def post(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT ts, uid, id, method, uri, status_code
+                    from MERGED
+                    WHERE method = 'POST'
+                    """)
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+ 
+def file_not_null(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT _path, tx_hosts, rx_hosts, conn_uids, mime_type, filename, md5, sha1
+                    from MERGED
+                    WHERE filename IS NOT NULL
+                    """)
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+    
+def count_path(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT _path, count(*)
+                    from MERGED
+                    GROUP BY _path
+                    ORDER BY COUNT(*) DESC""")
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+    
+def path_dns(df, iter_num):
+    origin = sys.stdout
+    df5 = spark.sql("""SELECT query, count(*)
+                    from MERGED
+                    WHERE _path = 'dns'
+                    GROUP BY query
+                    ORDER BY COUNT(*) DESC""")
+    sys.stdout = open('/dev/null', 'w')
+    df5.show()
+    sys.stdout = origin
+
+def http_reqs(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT DISTINCT `id.orig_h`, `id.resp_h`, `id.resp_p`, method, host, uri
+                    FROM MERGED
+                    WHERE _path = 'http'
+                    """)
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+
+def path_conn(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT DISTINCT `id.orig_h`, `id.resp_h`, `id.resp_h`
+                   FROM MERGED
+                   WHERE _path = 'conn'
+                   """)
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+
+def total_bytes(df, iter_num):
+    origin = sys.stdout
+    df2 = spark.sql("""SELECT orig_bytes + resp_bytes AS total_bytes, uid, orig_bytes, resp_bytes
+                    FROM MERGED
+                    WHERE _path = 'conn'
+                    ORDER BY total_bytes DESC""")
+    sys.stdout = open('/dev/null', 'w')
+    df2.show()
+    sys.stdout = origin
+
+def benchmark(fn, num_iter=100):
+    #global spark
     _real = list()
-    #_real, _sys, _user = list(), list(), list()
-    for _ in range(num_iter):
-        #_df = copy.deepcopy(df)
-        t = unix_time(fn, df=df)
-        _real.append(t["real"])
-        #_sys.append(t["sys"])
-        #_user.append(t["user"])
-        print("{}".format(fn.__name__))
-        print("{}".format(_real))
-       # print("{}".format(round(statistics.mean(_user), 5)))
-       # print("{}".format(round(statistics.mean(_sys), 5)))
-
+    for i in range(num_iter):
+       # os.system('sudo sh flush_cache.sh')
+        df = load()
+        if fn != analytics:
+            df = df.createOrReplaceTempView("MERGED") 
+        t = unix_time(fn, df=df, iter_num=i)
+        if i >= 10:
+            _real.append(t["real"])
+        #if fn == search and i == 0:
+            #df = t["return"]
+        if i == num_iter - 1:
+            print("{}".format(fn.__name__))
+            print("{}".format(_real))
+            print("{}".format(mean(_real)))
+        #spark.stop()
+        #time.sleep(5)
+        #os.system('ps aux | grep spark')
+        #spark = SparkSession.builder.master("local[1]").getOrCreate()
 
 def main():
     # TBD use sys.argv[0] for num_iter or dataset
+   
     print("loading..")
     #df = unix_time(load)["return"]
-    df = load()
-    print("name,real,user,sys")
+    #df = load()
+    print("name,real")
     print("------------------")
 
     print("Analytics query")
     print("count total number of records with each distinct source IP")
-    benchmark1(analytics, df, num_iter=10)
-    print("Search query")
-    print("find all records with IP 10.128.19, sort by timestamp and return top 5")
-    df = load()
-    df.createOrReplaceTempView("MERGED") 
-    benchmark1(search, df, num_iter=10)
+    #benchmark(analytics, num_iter=100)
+    
     print("Data discovery query")
     print("count the number of records with each different schema")
-    benchmark1(discovery, df, num_iter=10)
+    #benchmark(discovery, num_iter=100)
 
+    print("Windows Networking Activity")
+    #benchmark(path, num_iter=100)
+
+    print("HTTP Post Requests")
+    #benchmark(post, num_iter=100)
+
+    print("Activity overview")
+    #benchmark(count_path, num_iter=100)
+
+    print("File Activity")
+    #benchmark(file_not_null, num_iter=100)
+
+    print("Unique DNS queries")
+    #benchmark(path_dns, num_iter=100)
+
+    print("HTTP Requests")
+    #benchmark(http_reqs, num_iter=100)
+
+    print("Unique Network Connections")
+    #benchmark(path_conn, num_iter=100)
+
+    print("Connection Received Data")
+    #benchmark(total_bytes, num_iter=100)
+ 
+    print("Search query")
+    print("find all records with IP 10.128.19, sort by timestamp and return top 5")
+    #df.createOrReplaceTempView("MERGED") 
+    benchmark(search, num_iter=100)
+    
 if __name__ == '__main__':
         main()
 
