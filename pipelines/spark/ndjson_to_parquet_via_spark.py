36813888feab6f4222a26e2c41ed5aa947e4a71d
aousterh <amy.ousterhout@berkeley.edu>
2021-06-21 20:21:57 +0000

    spark: add a script for converting NDJSON to Parquet using Spark
    Yields files with a more recent version of Parquet for compatibility with zq.

---
+++ pipelines/spark/ndjson_to_parquet_via_spark.py
@@ -0,0 +1,25 @@
+import os
+from pyspark.sql.session import SparkSession
+
+# this script converts ndjson files from path ndjson_dir_name to parquet files stored in parquet_dir
+
+ndjson_dir_name = "/zq-sample-data/zeek-ndjson"
+parquet_dir = 'parquet'
+
+def ndjson_to_parquet():
+    os.system("rm -fr " + parquet_dir)
+    os.system("mkdir " + parquet_dir)
+
+    for root, dirs, files in os.walk(ndjson_dir_name, topdown=False):
+        for name in files:
+            ndjson_filename = os.path.join(root, name)
+            print("processing " + ndjson_filename)
+
+            # read ndjson into a dataframe, write out as parquet into parquet_dir
+            df = spark.read.json(ndjson_filename)
+            df.write.format("parquet").mode("append").save(parquet_dir)
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.master("local[1]").getOrCreate()
+
+    ndjson_to_parquet()
