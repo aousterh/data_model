8ea78e3e1645a1e0ce123261e0587981bab1428d
aousterh <amy.ousterhout@berkeley.edu>
2021-07-05 23:09:47 +0000

    benchmark: support ZST in type context microbenchmark

--- benchmark/bench_type_contexts.py
+++ benchmark/bench_type_contexts.py
@@ -13,6 +13,11 @@ n_records = 1000 * 1000
 n_data_points = 10
 results_dir = "type_context_results"
 
+# query, query description
+queries = [("by typeof(.) | count()", "by typeof count"),
+           ("count()", "count")]
+zed_formats = ["zng", "zst"]
+
 def main():
     os.system("rm -fr " + results_dir)
     os.system("mkdir " + results_dir)
@@ -39,28 +44,29 @@ def main():
     for n_types, f_name, f in zson_files:
         f.close()
 
-    # create zng files
-    for n_types, f_name, f in zson_files:
-        zng_name = f_name.split(".")[0] + ".zng"
-        os.system("zq -f zng -o " + zng_name + " " + f_name)
+    # create zng and zst files
+    for n_types, zson_name, f in zson_files:
+        for zed_format in zed_formats:
+            file_name = zson_name.split(".")[0] + "." + zed_format
+            os.system("zq -f " + zed_format + " -o " + file_name + " " + zson_name)
 
-    # benchmark how long it takes to count all the types in each zng file, etc.
+    # benchmark how long it takes to count all the types in each zng/zst file, etc.
     with open(results_dir + "/results.csv", "w") as f_results:
-        f_results.write("query,n_types,real,user,sys,us_per_type\n")
+        f_results.write("format,query,n_types,real,user,sys,us_per_type\n")
         for n_types, f_name, f in zson_files:
-            zng_name = f_name.split(".")[0] + ".zng"
 
-            # count types
-            results = unix_time_bash("zq -Z \"by typeof(.) | count()\" " + zng_name)
-            fields = ["by typeof count", n_types, results['real'], results['user'], results['sys'],
-                      "{:.2f}".format(results['real'] * US_PER_S / n_types)]
-            f_results.write(",".join(str(x) for x in fields) + "\n")
+            for zed_format in zed_formats:
+                file_name = f_name.split(".")[0] + "." + zed_format
 
-            # count records
-            results = unix_time_bash("zq -Z \"count()\" " + zng_name)
-            fields = ["count", n_types, results['real'], results['user'], results['sys'],
-                      "{:.2f}".format(results['real'] * US_PER_S / n_types)]
-            f_results.write(",".join(str(x) for x in fields) + "\n")
+                # issue queries
+                for query, description in queries:
+                    results = unix_time_bash("zq -i " + zed_format +
+                                             " -Z \"" + query + "\" " +
+                                             file_name)
+                    fields = [zed_format, description, n_types, results['real'],
+                              results['user'], results['sys'],
+                              "{:.2f}".format(results['real'] * US_PER_S / n_types)]
+                    f_results.write(",".join(str(x) for x in fields) + "\n")
 
 if __name__ == '__main__':
     main()
