c7991fb6b81346578175991d0a1bae406869d220
aousterh <amy.ousterhout@berkeley.edu>
2021-07-06 23:07:48 +0000

    benchmark: add parquet and sum query to microbenchmark

--- benchmark/bench_type_contexts.py
+++ benchmark/bench_type_contexts.py
@@ -3,32 +3,37 @@
 # run with taskset to restrict to one core!
 # taskset -c 1 ./bench_type_contexts.py
 
-from util import unix_time_bash
+from util import unix_time, unix_time_bash
 import os
 import random
 import string
+from pyspark.sql.functions import sum
+from pyspark.sql.session import SparkSession
 
 US_PER_S = 1000 * 1000
 n_records = 1000 * 1000
-n_data_points = 10
+factor = 8
 results_dir = "type_context_results"
 
+def parquet_dir(n_types):
+    return "{}/parquet_{:0>7}_types".format(results_dir, n_types)
+
 # query, query description
-queries = [("by typeof(.) | count()", "by typeof count"),
-           ("count()", "count")]
+zed_queries = [("by typeof(.) | count()", "by typeof count"),
+           ("count()", "count"),
+           ("sum(value)", "sum")]
 zed_formats = ["zng", "zst"]
 
-def bench(zed_file_info):
+def bench_zed(zed_file_info):
     # benchmark some queries over each file
-    with open(results_dir + "/results.csv", "w") as f_results:
-        f_results.write("format,query,n_types,real,user,sys,us_per_type\n")
+    with open(results_dir + "/results.csv", "a") as f_results:
         for n_types, base_name in zed_file_info:
 
             for zed_format in zed_formats:
                 file_name = base_name + "." + zed_format
 
                 # issue queries
-                for query, description in queries:
+                for query, description in zed_queries:
                     results = unix_time_bash("zq -i " + zed_format +
                                              " -Z \"" + query + "\" " +
                                              file_name)
@@ -37,9 +42,14 @@ def bench(zed_file_info):
                               "{:.2f}".format(results['real'] * US_PER_S / n_types)]
                     f_results.write(",".join(str(x) for x in fields) + "\n")
 
-def create_data():
-    type_range = [x for x in range(0, n_records + 1, int(n_records / n_data_points))]
-    type_range[0] = 1 # extreme case is 1 type, not 0
+def create_zed_data():
+    type_range = []
+    n = n_records
+    while n > 1:
+        type_range.insert(0, n)
+        n = int(n / factor)
+    type_range.insert(0, 1)
+    print("types: " + str(type_range))
 
     # info about zed data (num types, base file name)
     zed_file_info = []
@@ -57,6 +67,7 @@ def create_data():
     # but vary field names to create different numbers of types per file
     for i in range(n_records):
         letter = random.choice(string.ascii_letters)
+        value = random.randint(0, 1000)
 
         for n_types, base_name in zed_file_info:
             if n_types == 1:
@@ -65,7 +76,7 @@ def create_data():
                 type_name = i % n_types
 
             f = zson_files[base_name]
-            f.write("{{\"{:0>6}\":\"{}\"}}\n".format(type_name, letter))
+            f.write("{{\"{:0>6}\":\"{}\",value:{}}}\n".format(type_name, letter, value))
 
     for f in zson_files.values():
         f.close()
@@ -78,12 +89,68 @@ def create_data():
 
     return zed_file_info
 
+def create_parquet_data(zed_file_info):
+    spark = SparkSession.builder.master("local[1]").config("spark.executor.memory", "4g").getOrCreate()
+
+    # convert ZNG to NDJSON
+    for n_types, base_name in zed_file_info:
+        os.system("zq -f ndjson -o {}.ndjson {}.zng".format(base_name, base_name))
+
+    # convert NDJSON to Parquet
+    for n_types, base_name in zed_file_info:
+        # create directory for data with this many types
+        os.system("rm -fr {}".format(parquet_dir(n_types)))
+        os.system("mkdir {}".format(parquet_dir(n_types)))
+
+        # merges all types into a single uber schema
+        df = spark.read.json(base_name + ".ndjson")
+        df.write.format("parquet").mode("append").save(parquet_dir(n_types))
+
+def query_count(df):
+    df.count()
+
+def query_sum(df):
+    df.select(sum("value")).show()
+
+spark_queries = [(query_count, "count"),
+                 (query_sum, "sum")]
+
+def bench_parquet(zed_file_info):
+    spark = SparkSession.builder.master("local[1]").config("spark.executor.memory", "4g").getOrCreate()
+
+    with open(results_dir + "/results.csv", "a") as f_results:
+        for n_types, base_name in zed_file_info:
+            df = spark.read.parquet("{}/*.parquet".format(parquet_dir(n_types)))
+
+            for query, description in spark_queries:
+                results = unix_time(query, df=df)
+
+                results_str = {}
+                for k in ["real", "user", "sys"]:
+                    results_str[k] = "{:.2f}".format(results[k])
+
+                fields = ["parquet", description, n_types, results_str['real'],
+                          results_str['user'], results_str['sys'],
+                          "{:.2f}".format(results['real'] * US_PER_S / n_types)]
+                f_results.write(",".join(str(x) for x in fields) + "\n")
+
 def main():
     os.system("rm -fr " + results_dir)
     os.system("mkdir " + results_dir)
 
-    zed_file_info = create_data()
-    bench(zed_file_info)
+    with open(results_dir + "/results.csv", "w") as f_results:
+        f_results.write("format,query,n_types,real,user,sys,us_per_type\n")
+
+    zed_file_info = create_zed_data()
+    bench_zed(zed_file_info)
+
+    zed_file_info_subset = []
+    for n_types, base_name in zed_file_info:
+        if n_types < 5000:
+            zed_file_info_subset.append((n_types, base_name))
+    print("parquet subset: " + str(zed_file_info_subset))
+    create_parquet_data(zed_file_info_subset)
+    bench_parquet(zed_file_info_subset)
 
 if __name__ == '__main__':
     main()
