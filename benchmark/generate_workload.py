63ff4758c02a75aef012d4b0420fe7d74a24ce5a
aousterh <amy.ousterhout@berkeley.edu>
2021-07-29 19:16:14 +0000

    benchmark: update workload generation for consistency with directory structure, add shorter workload

--- benchmark/generate_workload.py
+++ benchmark/generate_workload.py
@@ -7,9 +7,9 @@ import os
 import pandas as pd
 import random
 
-DATA_DIR=os.getcwd()
-#DATA_DIR="/zq-sample-data/zeek-ndjson"
-OUTPUT_DIR="workload"
+#DATA_DIR=os.getcwd()
+DATA_DIR="/zq-sample-data/zeek-ndjson"
+OUTPUT_DIR="workload/trace"
 
 def getUnique(field):
     recordList =[]
@@ -32,7 +32,7 @@ def getUnique(field):
         df.to_csv(outfile, index=False, mode='a')
     return df[field]
 
-def generateWorkload(query_name, field="id.orig_h" ,runs=1000, seed=10):
+def generateWorkload(query_name, field="id.orig_h", runs=1000, seed=42):
     workload = []
     uniqueVals = getUnique(field)
     random.seed(seed)
@@ -40,15 +40,13 @@ def generateWorkload(query_name, field="id.orig_h" ,runs=1000, seed=10):
         uniqueVal = random.choice(uniqueVals)
         workload.append({'query': query_name + " " + field, 'arguments': [uniqueVal]})
 
-    with open('{}/workload.ndjson'.format(OUTPUT_DIR), 'w') as f:
+    with open('{}/network_log_search_{}.ndjson'.format(OUTPUT_DIR, runs), 'w') as f:
         writer = ndjson.writer(f, ensure_ascii=False)
         for query in workload:
             writer.writerow(query)
 
 def main():
-    os.system("rm -fr {}".format(OUTPUT_DIR))
-    os.system("mkdir {}".format(OUTPUT_DIR))
-    generateWorkload("search", "id.orig_h", 1000, 10)
+    generateWorkload("search", "id.orig_h", 30)
 
 if __name__ == "__main__":
     main()
