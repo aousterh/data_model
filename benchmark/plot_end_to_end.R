b6f0c27f283956e0a71afa7324e8bf125ebc1995
Amy Ousterhout <amy.ousterhout@berkeley.edu>
2021-07-30 00:47:24 -0700

    benchmark: script for plotting search query results

---
+++ benchmark/plot_end_to_end.R
@@ -0,0 +1,63 @@
+#!/usr/bin/env Rscript
+# This script can be run using:
+# ./plot_end_to_end.R <data0> <data1> <data2> <data3> <data4>
+
+# use the ggplot2 library
+library(ggplot2)
+library(plyr)
+
+# read in data
+args <- commandArgs(trailingOnly=TRUE)
+
+data0 <- read.csv(args[1], sep=",")
+data1 <- read.csv(args[2], sep=",")
+data2 <- read.csv(args[3], sep=",")
+data3 <- read.csv(args[4], sep=",")
+data4 <- read.csv(args[5], sep=",")
+
+data <- rbind.fill(data0, data1, data2, data3, data4)
+
+head(data)
+
+theme_set(theme_bw(base_size=12))
+
+
+ggplot(data, aes(x=index, y=real, color=interaction(system, in_format))) +
+	     geom_point() +
+	     geom_line() +
+	     labs(x="Query Number", y="Query Time (s)") +
+	     scale_color_discrete(name="System.Format")
+
+ggsave("query_time_by_index.pdf", width=7, height=6)
+
+
+ggplot(data, aes(x=index, y=real, color=interaction(system, in_format))) +
+	     geom_point() +
+	     geom_line() +
+	     labs(x="Query Number", y="Query Time (s)") +
+	     scale_color_discrete(name="System.Format") +
+	     coord_cartesian(ylim=c(0, 2.2))
+
+ggsave("query_time_by_index_zoom.pdf")
+
+
+data_skip_warmup = data[data$index >= 10,]
+ggplot(data_skip_warmup, aes(real, color=interaction(system, in_format))) +
+	stat_ecdf() +
+	labs(x="Query Time (s)", y="CDF") +
+	scale_color_discrete(name="System.Format")
+
+ggsave("query_time_cdf.pdf")
+
+
+ggplot(data_skip_warmup, aes(x=validation, y=real, color=interaction(system, in_format))) +
+			 geom_point() +
+			 stat_smooth(method='lm', aes(color=interaction(system, in_format)), se=FALSE, size=0.5) +
+			 labs(x="Number of Matching Records", y="Query Time (s)") +
+			 scale_color_discrete(name="System.Format")
+
+ggsave("query_time_by_num_results.pdf")
+
+
+summary(data$validation)
+tapply(data$real, interaction(data$system, data$in_format), summary)
