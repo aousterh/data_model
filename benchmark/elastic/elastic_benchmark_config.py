947492e8af0952da3f33cc818dee05afc657104e
Ananya Krishnaswamy <ananyaks@berkeley.edu>
2021-08-13 17:38:12 +0000

    fixed config file to run in repo

--- benchmark/elastic/elastic_benchmark_config.py
+++ benchmark/elastic/elastic_benchmark_config.py
@@ -4,7 +4,7 @@ WORKLOAD_FILE_NAME="network_log_search_30.ndjson"
 
 OUTPUT_DIR="elastic-output/"
 
-OUTPUT_FILE_NAME="query_execution_times.csv"
+OUTPUT_FILE_NAME="elastic_network_log_search_30_execution_times.csv"
 
 QUERIES = ["search id.orig_h", 
            "search id.orig_h + sort ts", 
