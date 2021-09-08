09d6ed2a80c31d19a407789aa4b88685ab3a978a
silveryfu <silveryfu@gmail.com>
2021-09-08 01:30:47 -0700

    mongo: add search/sort/head and avg queries

--- benchmark/mongo/benchmark.py
+++ benchmark/mongo/benchmark.py
@@ -36,64 +36,64 @@ class Benchmark:
         if self._meta.get("warmup", True):
             raise NotImplemented
 
-        for workload in self._benchmark:
-            wc = util.workload_config(workload)
+        for workload, queries in self._benchmark.items():
+            for name in queries:
+                qc = util.workload_config(workload, query=name)
 
-            for name, param in wc["query"].items():
                 start = time.time()
                 results = list()
                 query_funcs = list()
 
-                _update_index(self._db, self._cols, param["field"],
-                              drop=not param.get("index", False))
+                if "index" in qc:
+                    _drop = not qc.get("index", False)
+                    _update_index(self._db, self._cols,
+                                  qc["field"],
+                                  drop=_drop)
 
-                if wc["kind"] == "search":
+                if name in {"search", "search_sort_head"}:
+                    sort_head = True if name == "search_sort_head" else False
                     def make_exec(_v):
                         def _exec():
                             if len(self._cols) > 1:
                                 with Pool(self._meta.get("num_thread", 1)) as pool:
-                                    _output = pool.starmap(_search, [(self._db, c, param["field"], _v)
+                                    _output = pool.starmap(_search, [(self._db, c, qc["field"], _v, sort_head)
                                                                      for c in self._cols])
                             else:
-                                _output = _search(self._db, self._cols[0], param["field"], _v)
+                                _output = _search(self._db, self._cols[0], qc["field"], _v, sort_head)
                             return _output
 
                         return _exec
 
                     values = list()
-                    tf = param.get("trace_file", None)
+                    tf = qc.get("trace_file", None)
                     if tf:
                         values = [row["arguments"][0] for row in util.read_trace(tf)]
-                        values = values[:param.get("batch_size", len(values))]
+                        values = values[:qc.get("batch_size", len(values))]
                     for v in values:
                         query_funcs.append((make_exec(v), v))
 
-                elif wc["kind"] == "analytics":
+                elif name == "avg":
                     # TBD
-                    def make_exec(_s, _e):
+                    def make_exec(_v):
                         def _exec():
                             if len(self._cols) > 1:
                                 with Pool(self._meta.get("num_thread", 1)) as pool:
-                                    _output = pool.starmap(_range_sum, [(self._db, c,
-                                                                         param["field"],
-                                                                         param["target"], _s, _e)
-                                                                        for c in self._cols])
+                                    _output = pool.starmap(_avg, [(self._db, c, _v)
+                                                                  for c in self._cols])
                             else:
-                                _output = _range_sum(self._db, self._cols[0],
-                                                     param["field"],
-                                                     param["target"], _s, _e)
+                                _output = _avg(self._db, self._cols[0], _v)
                             return _output
 
                         return _exec
 
                     values = list()
-                    tf = param.get("trace_file", None)
+                    tf = qc.get("trace_file", None)
                     if tf:
-                        values = [row["arguments"] for row in util.read_trace(tf)]
-                        values = values[:param.get("batch_size", len(values))]
-                    for _start, _end in values:
-                        query_funcs.append((make_exec(_start, _end), f"{_start} {_end}"))
-                else:
+                        values = [row["arguments"][0] for row in util.read_trace(tf)]
+                        values = values[:qc.get("batch_size", len(values))]
+                    for v in values:
+                        query_funcs.append((make_exec(v), v))
+                elif name == "search_sort_head":
                     raise NotImplemented
 
                 for i, (f, arg) in enumerate(query_funcs):
@@ -104,18 +104,30 @@ class Benchmark:
                     results.append(OrderedDict({
                         "index": i,
                         "system": "mongo",
-                        "in_format": "index" if param.get("index", False) else "bson",
+                        "in_format": "index" if qc.get("index", False) else "bson",
                         "out_format": "json",
-                        "query": param["desc"],
+                        "query": qc["desc"],
                         "start_time": round(time.time() - start, 3),
                         "real": r["real"],
                         "user": r["user"],
                         "sys": r["sys"],
                         "argument_0": arg,
-                        "validation": len(r["return"]) if wc["kind"] == "search" else r["return"],
+                        "validation": {
+                            "search": lambda x: len(x),
+                            "search_sort_head": lambda x: x[-1]["id"]["orig_p"],
+                        }.get(name, lambda x: x)(r["return"]),
                         "instance": self._meta.get("instance", "unknown"),
                     }))
-            util.write_csv(results, f"mongo-{wc['kind']}-{name}{'-index' if param.get('index', False) else ''}.csv")
+                util.write_csv(results, f"mongo-{name}{'-index' if qc.get('index', False) else ''}.csv")
+
+
+def _avg(db, col, field):
+    _c = MongoClient('localhost', 27017, maxPoolSize=10000)
+
+    r = _c[db][col].aggregate(
+        [{"$group": {"_id": None, "avg": {"$avg": f"${field}"}}}]
+    )
+    return list(r)[0]["avg"]
 
 
 def _range_sum(db, col, field, target, start, end):
@@ -141,8 +153,10 @@ def _update_index(db, cols, field, drop=False):
         print(e)
 
 
-def _search(db, col, field, value):
+def _search(db, col, field, value, sort_head):
     _c = MongoClient('localhost', 27017, maxPoolSize=10000)
+    if sort_head:
+        return list(_c[db][col].find({field: value}).sort("ts").limit(1000))
     return list(_c[db][col].find({field: value}))
 
 
