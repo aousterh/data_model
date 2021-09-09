84c6628d4c816fbe44a538579658f38bb7a2614b
Ananya Krishnaswamy <ananyaks@berkeley.edu>
2021-09-09 21:11:16 +0000

    updated script to use search after

--- benchmark/elastic/elastic_benchmark_config.py
+++ benchmark/elastic/elastic_benchmark_config.py
@@ -6,14 +6,9 @@ OUTPUT_DIR="elastic-output/"
 
 OUTPUT_FILE_NAME="elastic_network_log_search_30_execution_times.csv"
 
-QUERIES = ["search id.orig_h", 
-           "search id.orig_h + sort ts", 
-           "search id.orig_h + sort ts + slice 5",
-           "search id.orig_h + count by id.resp_h",
-           "search id.orig_h + sum orig_bytes",
-           "search id.orig_h + count by schema"]
+QUERIES = ["search id.orig_h","search id.orig_h + sort ts + slice 1000", "analytics avg orig_bytes"]
 
 
-AGGREGATION_FIELDS= {"search id.orig_h + count by id.resp_h": "id.resp_h", 
-					 "search id.orig_h + count by schema": "_path" }
+AGGREGATION_FIELDS= {"search id.orig_h + count by id.resp_h": "id.resp_h",
+                                                 "search id.orig_h + count by schema": "_path" }
 
--- benchmark/elastic/issue_elastic_queries.py
+++ benchmark/elastic/issue_elastic_queries.py
@@ -16,28 +16,44 @@ aggregation_fields = AGGREGATION_FIELDS
 def runQuery(query):
     cmd = query + " > " + output_directory + "query-output.json"
     os.system(cmd)
+
+    sortvals = []
     with open(output_directory + "query-output.json", 'r') as j:
         contents = json.loads(j.read())
         executiontime = contents["took"]
         hits = contents["hits"]["total"]["value"]
-
+        ret = {"executiontime": executiontime * 0.001, "hits": hits, "sortval": 0}
+        for content in contents["hits"]["hits"]:
+            sortvals.append(content["sort"])
+        if sortvals:
+            sortval = sortvals.pop()[0]
+            if sortval:
+                ret["sortval"] = sortval
     os.system("rm " + output_directory + "query-output.json")
-    return {"executiontime": executiontime * 0.001, "hits": hits}
+    return ret
 
-def getQuery(queryname, arguments):
+def getQuery(queryname, arguments, sortval):
     # TODO: edit queries to return all results, not just top 10-1000
     if queryname == "search id.orig_h":
-        query = "curl -X GET 'http://localhost:9200/test/_search?q=id.orig_h:"+arguments[0]+"&format=json&pretty'"
-    elif queryname == "search id.orig_h + sort ts":
-        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"sort\" : [\"ts\" ],\"query\" : {\"term\" : { \"id.orig_h\" : \""+arguments[0]+"\" }}}'"
-    elif queryname == "search id.orig_h + sort ts + slice 5":
-        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"size\": 5,\"sort\" : [{\"ts\" : {\"order\": \"asc\" }}],\"query\" : {\"term\" : { \"id.orig_h\" : \""+arguments[0]+"\" }}}'"
-    elif queryname == "search id.orig_h + count by id.resp_h":
-        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"id.resp_h\": {\"terms\": {\"field\": \"id.resp_h\"}}}}'"
-    elif queryname == "search id.orig_h + sum orig_bytes":
-        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"orig_bytes\": {\"sum\": {\"field\": \"orig_bytes\"}}}}'"
-    elif queryname == "search id.orig_h + count by schema":
-        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"schema\": {\"terms\": {\"field\": \"_path\"}}}}'"
+        if sortval:
+            query = "curl -X GET \"localhost:9200/test/_search?pretty\" -H 'Content-Type: application/json' -d' { \"search_after\": ["+str(sortval)+"] , \"sort\": [{\"ts\": \"asc\"}],\"query\" : {\"term\" : {\"id.orig_h\" : \""+arguments[0]+"\" } } } '"
+        else:
+            query = "curl -X GET \"localhost:9200/test/_search?pretty\" -H 'Content-Type: application/json' -d' {  \"sort\": [{\"ts\": \"asc\"}], \"query\" : {\"term\" : { \"id.orig_h\" : \""+arguments[0]+"\" } } } '"
+    #elif queryname == "search id.orig_h + sort ts":
+    #     query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"sort\" : [\"ts\" ],\"query\" : {\"term\" : { \"id.orig_h\" : \""+arguments[0]+"\" }}}'"
+    elif queryname == "search id.orig_h + sort ts + slice 1000":
+        query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"size\": 1000,\"sort\" : [{\"ts\" : {\"order\": \"asc\" }}],\"query\" : {\"term\" : { \"id.orig_h\" : \""+arguments[0]+"\" }}}'"
+    # elif queryname == "search id.orig_h + count by id.resp_h":
+    #     query  = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"id.resp_h\": {\"terms\": {\"field\": \"id.resp_h\"}}}}'"
+    # elif queryname == "search id.orig_h + sum orig_bytes":
+    #     query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"orig_bytes\": {\"sum\": {\"field\": \"orig_bytes\"}}}}'"
+    # elif queryname == "search id.orig_h + count by schema":
+    #     query = "curl -X GET \"localhost:9200/test/_search?format=json&pretty\" -H 'Content-Type: application/json' -d'{\"query\" : {\"term\" : { \"id.orig_h\" : \""+ arguments[0] +"\" }}, \"aggs\": {\"schema\": {\"terms\": {\"field\": \"_path\"}}}}'"
+    elif queryname == "analytics avg orig_bytes":
+        if sortval:
+            query = "curl -X GET \"localhost:9200/test/_search?pretty\" -H 'Content-Type: application/json' -d'{ \"sort\": [{\"ts\": \"asc\"}], \"search_after\": ["+str(sortval)+"], \"aggs\": {\"time\": {\"date_range\": {\"field\": \"ts\",\"ranges\": [{\"from\": \""+arguments[0]+"\", \"to\": \""+arguments[1]+"\"}]},\"aggs\": {\"avg_orig_bytes\": {\"avg\": { \"field\": \"orig_bytes\"}}}}}}'"
+        else:
+            query = "curl -X GET \"localhost:9200/test/_search?pretty\" -H 'Content-Type: application/json' -d'{ \"sort\": [{\"ts\": \"asc\"}], \"aggs\": {\"time\": {\"date_range\": {\"field\": \"ts\",\"ranges\": [{\"from\": \""+arguments[0]+"\", \"to\": \""+arguments[1]+"\"}]},\"aggs\": {\"avg_orig_bytes\": {\"avg\": { \"field\": \"orig_bytes\"}}}}}}'"
     else:
         query= ""
     return query
@@ -47,24 +63,40 @@ def issueQueries():
     query_name_list = []
     argument_list = []
     validation_list =[]
-    
-    restartElastic()
+    sortval_list=[]
+
+    #restartElastic()
 
     with open(input_directory + workload_file ) as f:
         data = ndjson.load(f)
         for d in data:
             if (d['query'] in queries):
                 queryname = d['query']
-                query = getQuery(queryname, d['arguments'])
+                query = getQuery(queryname, d['arguments'], 0)
                 if query:
                     if queryname in aggregation_fields:
                         prepForAggregation(aggregation_fields[queryname])
 
                     queryoutput = runQuery(query)
+                    sortval = queryoutput['sortval']
+                    indiv_hits = queryoutput['hits']
+
+                    while indiv_hits >= 10000 and sortval:
+                        sa_query = getQuery(queryname, d['arguments'], sortval)
+                        curr_queryoutput = runQuery(sa_query)
+
+                        queryoutput["executiontime"] = queryoutput["executiontime"] + curr_queryoutput["executiontime"]
+                        queryoutput["hits"] = queryoutput["hits"] + curr_queryoutput["hits"]
+                        queryoutput["sortval"] = curr_queryoutput["sortval"]
+
+                        sortval = curr_queryoutput["sortval"]
+                        indiv_hits = curr_queryoutput["hits"]
+
                     real_list.append(queryoutput["executiontime"])
                     query_name_list.append(d['query'])
                     argument_list.append(d['arguments'])
                     validation_list.append(queryoutput["hits"])
+                    sortval_list.append(queryoutput["sortval"])
 
                     if queryname in aggregation_fields:
                         cleanFromAggregation(aggregation_fields[queryname])
@@ -72,7 +104,7 @@ def issueQueries():
                     print('Query Not Supported: ' + str(d['query']))
             else:
                 print('Query Not Supported: ' + str(d['query']))
-     	
+
     num_queries = len(real_list)
     output_df = pd.DataFrame({'index': range(num_queries)})
     output_df = output_df.set_index('index')
@@ -87,6 +119,8 @@ def issueQueries():
     output_df.insert(7, 'sys',[np.nan] * num_queries)
     output_df.insert(8, 'argument_0', argument_list)
     output_df.insert(9, 'validation', validation_list)
+    output_df.insert(10, 'sortval', sortval_list)
+
 
     output_df.to_csv(output_directory + output_file, na_rep='NaN')
     return output_df
@@ -98,10 +132,10 @@ def restartElastic():
     os.system("sleep 30")
 
 
-# Aggregation prep/clean due to: 
-# "Text fields are not optimised for operations that require per-document field data like aggregations and sorting, so these operations are disabled by default. 
-# Please use a keyword field instead. 
-# Alternatively, set fielddata=true on [id.resp_h] in order to load field data by uninverting the inverted index. 
+# Aggregation prep/clean due to:
+# "Text fields are not optimised for operations that require per-document field data like aggregations and sorting, so these operations are disabled by default.
+# Please use a keyword field instead.
+# Alternatively, set fielddata=true on [id.resp_h] in order to load field data by uninverting the inverted index.
 # Note that this can use significant memory."
 
 def prepForAggregation(fieldname):
